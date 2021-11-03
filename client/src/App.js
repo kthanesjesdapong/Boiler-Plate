130e99d05e8ecee207de821d9e14d442cc4b45f8
Kavin Thanesjesdapong <KavinThanesjesdapong@gmail.com>
2021-11-02 20:51:35 -0700

    boiler-plate for futher project completed

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [resp, setResponse] = useState('');
+  useEffect(() => {
+    const getResponse = async () => {
+      const { data: response } = await axios.get('/api/hello');
+      setResponse(response);
+    };
+    getResponse();
+  }, []);
+  console.log(resp);
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>hello</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
